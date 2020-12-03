c7ee789ce677adc517a855e4683e2d29b4094322
datasciencedotuniversity <datasciencedotuniversity@gmail.com>
2020-12-03 12:26:31 +0100

    Security group added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Ok, I got it right!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
